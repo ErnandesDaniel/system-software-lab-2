3432744b370628f049ac706d7389d02c10fa082c
DN3672 <ernandes20@yandex.ru>
2025-11-10 22:39:42 +0300

    настроено отображение имен переменных при выводе

--- utils/compiler-utils/cfg/mermaid.c
+++ utils/compiler-utils/cfg/mermaid.c
@@ -5,6 +5,29 @@
 #include "types.h"
 #include "mermaid.h"
 
+// Вспомогательная функция для форматирования операнда
+void format_operand(const Operand* op, char* buffer, size_t size) {
+    if (!op || !buffer || size == 0) {
+        strncpy(buffer, "?", size - 1);
+        buffer[size - 1] = '\0';
+        return;
+    }
+
+    if (op->kind == OPERAND_VAR) {
+        snprintf(buffer, size, "%s", op->data.var.name);
+    } else if (op->kind == OPERAND_CONST) {
+        if (op->data.const_val.type->kind == TYPE_BOOL) {
+            snprintf(buffer, size, "%s", op->data.const_val.value.integer ? "true" : "false");
+        } else if (op->data.const_val.type->kind == TYPE_STRING) {
+            snprintf(buffer, size, "\"%s\"", op->data.const_val.value.string);
+        } else {
+            snprintf(buffer, size, "%d", (int)op->data.const_val.value.integer);
+        }
+    } else {
+        snprintf(buffer, size, "?");
+    }
+}
+
 // Преобразует IRInstruction в строку для отображения
 void format_ir_instruction(const IRInstruction* inst, char* buffer, size_t size) {
     if (!inst || !buffer || size == 0) {
@@ -14,22 +37,114 @@ void format_ir_instruction(const IRInstruction* inst, char* buffer, size_t size)
     }
 
     switch (inst->opcode) {
-        case IR_ADD: snprintf(buffer, size, "t = a + b"); break;
-        case IR_SUB: snprintf(buffer, size, "t = a - b"); break;
-        case IR_MUL: snprintf(buffer, size, "t = a * b"); break;
-        case IR_DIV: snprintf(buffer, size, "t = a / b"); break;
-        case IR_EQ:  snprintf(buffer, size, "t = (a == b)"); break;
-        case IR_NE:  snprintf(buffer, size, "t = (a != b)"); break;
-        case IR_LT:  snprintf(buffer, size, "t = (a < b)"); break;
-        case IR_LE:  snprintf(buffer, size, "t = (a <= b)"); break;
-        case IR_GT:  snprintf(buffer, size, "t = (a > b)"); break;
-        case IR_GE:  snprintf(buffer, size, "t = (a >= b)"); break;
-        case IR_AND: snprintf(buffer, size, "t = a && b"); break;
-        case IR_OR:  snprintf(buffer, size, "t = a || b"); break;
-        case IR_NOT: snprintf(buffer, size, "t = !a"); break;
-        case IR_NEG: snprintf(buffer, size, "t = -a"); break;
-        case IR_POS: snprintf(buffer, size, "t = +a"); break;
-        case IR_BIT_NOT: snprintf(buffer, size, "t = ~a"); break;
+        case IR_ADD: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = %s + %s", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_SUB: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = %s - %s", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_MUL: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = %s * %s", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_DIV: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = %s / %s", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_EQ: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = (%s == %s)", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_NE: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = (%s != %s)", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_LT: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = (%s < %s)", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_LE: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = (%s <= %s)", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_GT: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = (%s > %s)", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_GE: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = (%s >= %s)", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_AND: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = %s && %s", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_OR: {
+            char op1[64], op2[64];
+            format_operand(&inst->data.compute.operands[0], op1, sizeof(op1));
+            format_operand(&inst->data.compute.operands[1], op2, sizeof(op2));
+            snprintf(buffer, size, "%s = %s || %s", inst->data.compute.result, op1, op2);
+            break;
+        }
+        case IR_NOT: {
+            char op[64];
+            format_operand(&inst->data.unary.operand, op, sizeof(op));
+            snprintf(buffer, size, "%s = !%s", inst->data.unary.result, op);
+            break;
+        }
+        case IR_NEG: {
+            char op[64];
+            format_operand(&inst->data.unary.operand, op, sizeof(op));
+            snprintf(buffer, size, "%s = -%s", inst->data.unary.result, op);
+            break;
+        }
+        case IR_POS: {
+            char op[64];
+            format_operand(&inst->data.unary.operand, op, sizeof(op));
+            snprintf(buffer, size, "%s = +%s", inst->data.unary.result, op);
+            break;
+        }
+        case IR_BIT_NOT: {
+            char op[64];
+            format_operand(&inst->data.unary.operand, op, sizeof(op));
+            snprintf(buffer, size, "%s = ~%s", inst->data.unary.result, op);
+            break;
+        }
         case IR_ASSIGN: {
             if (inst->data.assign.value.kind == OPERAND_VAR) {
                 snprintf(buffer, size, "%s = %s",
@@ -55,9 +170,27 @@ void format_ir_instruction(const IRInstruction* inst, char* buffer, size_t size)
             break;
         }
         case IR_CALL: {
-            snprintf(buffer, size, "%s = call %s(...)",
-                     inst->data.call.result[0] ? inst->data.call.result : "_",
-                     inst->data.call.func_name);
+            char args[256] = {0};
+            char* ptr = args;
+            size_t remaining = sizeof(args);
+            for (int i = 0; i < inst->data.call.num_args; i++) {
+                char arg[64];
+                format_operand(&inst->data.call.args[i], arg, sizeof(arg));
+                int written = snprintf(ptr, remaining, "%s%s", arg, (i < inst->data.call.num_args - 1) ? ", " : "");
+                if (written < 0 || (size_t)written >= remaining) break;
+                ptr += written;
+                remaining -= written;
+            }
+            if (inst->data.call.result[0]) {
+                snprintf(buffer, size, "%s = call %s(%s)",
+                         inst->data.call.result,
+                         inst->data.call.func_name,
+                         args);
+            } else {
+                snprintf(buffer, size, "call %s(%s)",
+                         inst->data.call.func_name,
+                         args);
+            }
             break;
         }
         case IR_JUMP: {
@@ -65,15 +198,19 @@ void format_ir_instruction(const IRInstruction* inst, char* buffer, size_t size)
             break;
         }
         case IR_COND_BR: {
+            char cond[64];
+            format_operand(&inst->data.cond_br.condition, cond, sizeof(cond));
             snprintf(buffer, size, "if %s goto %s else %s",
-                     "cond", // можно улучшить, если хранить имя условия
+                     cond,
                      inst->data.cond_br.true_target,
                      inst->data.cond_br.false_target);
             break;
         }
         case IR_RET: {
             if (inst->data.ret.has_value) {
-                snprintf(buffer, size, "return %s", "value");
+                char val[64];
+                format_operand(&inst->data.ret.value, val, sizeof(val));
+                snprintf(buffer, size, "return %s", val);
             } else {
                 snprintf(buffer, size, "return");
             }
